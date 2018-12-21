9bdb8510110699de9faf986a25d506ca883fe304
Ryan May <xtheraingoddessx@gmail.com>
2018-12-20 19:47:00 -0800

    Main class init

---
+++ src/com/rain/main/Main.java
@@ -0,0 +1,14 @@
+package com.rain.main;
+
+public class Main {
+
+	public Main() {
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Marysol! I love you!");
+
+	}
+
+}
